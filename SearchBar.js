8e6a36fa15f4883186746cdfc015db383c741500
Nuno Campos <nuno.campos@me.com>
2017-05-21 23:05:06 +0100

    Remove use of deprecated React.PropTypes and React.createClass

--- SearchBar.js
+++ SearchBar.js
@@ -8,52 +8,20 @@ var TextInputState = require('react-native/lib/TextInputState');
 
 RNSearchBar = ReactNative.requireNativeComponent('RNSearchBar', null);
 
-PropTypes = React.PropTypes;
+PropTypes = require('prop-types');
 
 NativeModules = ReactNative.NativeModules;
 
-SearchBar = React.createClass({
-  propTypes: {
-    placeholder: PropTypes.string,
-    text: PropTypes.string,
-    barTintColor: PropTypes.string,
-    tintColor: PropTypes.string,
-    textColor: PropTypes.string,
-    textFieldBackgroundColor: PropTypes.string,
-    showsCancelButton: PropTypes.bool,
-    onChange: PropTypes.func,
-    onChangeText: PropTypes.func,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    onSearchButtonPress: PropTypes.func,
-    onCancelButtonPress: PropTypes.func,
-    enablesReturnKeyAutomatically: PropTypes.bool,
-    hideBackground: PropTypes.bool,
-    barStyle: PropTypes.oneOf(['default', 'black']),
-    searchBarStyle: PropTypes.oneOf(['default', 'prominent', 'minimal']),
-    editable: PropTypes.bool,
-    keyboardType: PropTypes.oneOf(['default', 'email-address', 'numeric', 'phone-pad', 'ascii-capable', 'numbers-and-punctuation', 'url', 'number-pad', 'name-phone-pad', 'decimal-pad', 'twitter', 'web-search']),
-    keyboardAppearance: PropTypes.oneOf(['default', 'light', 'dark']),
-    returnKeyType: PropTypes.oneOf(['done', 'go', 'next', 'search', 'send', 'default', 'emergency-call', 'google', 'join', 'route', 'yahoo']),
-    autoCapitalize: PropTypes.oneOf(['none', 'sentences', 'words', 'characters']),
-    autoCorrect: PropTypes.bool,
-    spellCheck: PropTypes.bool
-  },
-  getDefaultProps: function() {
-    return {
-      barStyle: 'default',
-      searchBarStyle: 'default',
-      editable: true
-    };
-  },
-  _onChange: function(e) {
+class SearchBar extends React.Component {
+  _onChange(e) {
     var base, base1;
     if (typeof (base = this.props).onChange === "function") {
       base.onChange(e);
     }
     return typeof (base1 = this.props).onChangeText === "function" ? base1.onChangeText(e.nativeEvent.text) : void 0;
-  },
-  _onPress: function(e) {
+  }
+
+  _onPress(e) {
     var base, base1, button;
     button = e.nativeEvent.button;
     if (button === 'search') {
@@ -61,30 +29,37 @@ SearchBar = React.createClass({
     } else if (button === 'cancel') {
       return typeof (base1 = this.props).onCancelButtonPress === "function" ? base1.onCancelButtonPress() : void 0;
     }
-  },
-  _onFocus: function() {
+  }
+
+  _onFocus() {
     TextInputState._currentlyFocusedID = ReactNative.findNodeHandle(this);
     if (this.props.onFocus) this.props.onFocus.apply(null, arguments);
-  },
-  _onBlur: function() {
+  }
+
+  _onBlur() {
     TextInputState._currentlyFocusedID = null;
     if (this.props.onBlur) this.props.onBlur.apply(null, arguments);
-  },
-  blur: function() {
+  }
+
+  blur() {
     return NativeModules.RNSearchBarManager.blur(ReactNative.findNodeHandle(this));
-  },
-  focus: function() {
+  }
+
+  focus() {
     return NativeModules.RNSearchBarManager.focus(ReactNative.findNodeHandle(this));
-  },
-  unFocus: function() {
+  }
+
+  unFocus() {
     return NativeModules.RNSearchBarManager.unFocus(ReactNative.findNodeHandle(this));
-  },
+  }
+
   componentWillReceiveProps (nextProps) {
     if (nextProps.showsCancelButton !== this.props.showsCancelButton) {
       NativeModules.RNSearchBarManager.toggleCancelButton(ReactNative.findNodeHandle(this), nextProps.showsCancelButton);
     }
-  },
-  render: function() {
+  }
+
+  render() {
     return <RNSearchBar
       style={{height: NativeModules.RNSearchBarManager.ComponentHeight}}
       onChange={this._onChange}
@@ -95,5 +70,38 @@ SearchBar = React.createClass({
     />;
   }
 });
+
+SearchBar.propTypes = {
+  placeholder: PropTypes.string,
+  text: PropTypes.string,
+  barTintColor: PropTypes.string,
+  tintColor: PropTypes.string,
+  textColor: PropTypes.string,
+  textFieldBackgroundColor: PropTypes.string,
+  showsCancelButton: PropTypes.bool,
+  onChange: PropTypes.func,
+  onChangeText: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onSearchButtonPress: PropTypes.func,
+  onCancelButtonPress: PropTypes.func,
+  enablesReturnKeyAutomatically: PropTypes.bool,
+  hideBackground: PropTypes.bool,
+  barStyle: PropTypes.oneOf(['default', 'black']),
+  searchBarStyle: PropTypes.oneOf(['default', 'prominent', 'minimal']),
+  editable: PropTypes.bool,
+  keyboardType: PropTypes.oneOf(['default', 'email-address', 'numeric', 'phone-pad', 'ascii-capable', 'numbers-and-punctuation', 'url', 'number-pad', 'name-phone-pad', 'decimal-pad', 'twitter', 'web-search']),
+  keyboardAppearance: PropTypes.oneOf(['default', 'light', 'dark']),
+  returnKeyType: PropTypes.oneOf(['done', 'go', 'next', 'search', 'send', 'default', 'emergency-call', 'google', 'join', 'route', 'yahoo']),
+  autoCapitalize: PropTypes.oneOf(['none', 'sentences', 'words', 'characters']),
+  autoCorrect: PropTypes.bool,
+  spellCheck: PropTypes.bool
+};
+
+SearchBar.defaultProps = {
+  barStyle: 'default',
+  searchBarStyle: 'default',
+  editable: true
+};
 
 module.exports = SearchBar;
